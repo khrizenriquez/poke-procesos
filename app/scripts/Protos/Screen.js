f4bd9e4aaffba9f80dc6ec0237e1e183976076e0
Chris Enríquez <christoferen7@gmail.com>
2016-08-14 23:53:42 -0600

    Cambio de let por var, excepto en el prototipo para dibujar a los pokémons

--- app/scripts/Protos/Screen.js
+++ app/scripts/Protos/Screen.js
@@ -6,14 +6,14 @@ var Screen = function () {
 }
 
 Screen.prototype.pressKey = function () {
-	let arrowDown = this.arrowDown
+	var arrowDown = this.arrowDown
 	document.addEventListener('click', function (e) {
 		if (winner) return false
 
 		guys.some(function (element, index, arr) {
 			if (parseInt(user.id) !== element.id) return
 
-			let top = document.querySelector('.canvasContainer').children[index].children[0].style.top
+			var top = document.querySelector('.canvasContainer').children[index].children[0].style.top
 			if (top == '') {
 				document.querySelector('.canvasContainer').children[index].children[0].style.top = '-10%'
 			} else {
@@ -24,14 +24,14 @@ Screen.prototype.pressKey = function () {
 	document.addEventListener('keyup', function (e) {
 		if (winner) return false
 
-		let key = e.keyCode || e.which
+		var key = e.keyCode || e.which
 
 		if (key !== arrowDown) return false
 
 		guys.some(function (element, index, arr) {
 			if (parseInt(user.id) !== element.id) return
 
-			let top = document.querySelector('.canvasContainer').children[index].children[0].style.top
+			var top = document.querySelector('.canvasContainer').children[index].children[0].style.top
 			if (top == '') {
 				document.querySelector('.canvasContainer').children[index].children[0].style.top = '-10%'
 			} else {
@@ -52,7 +52,7 @@ Screen.prototype.drawGuys = function(size) {
 		img
 
 	for (i; i < c.length; i++) {
-		let content = c[i]
+		var content = c[i]
 		img 		= new Image()
 		img.src 	= guys[i].img
 		img.onload = function () {
